bfd1af957ce641c795f4ad0567a571697704a0d8
Kshitij0169 <tiwari.ks@northeastern.edu>
2024-11-14 11:14:15 -0500

    Adding loading state and theme updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import React, { ReactNode } from "react";
 import "./_app";
+import { Select as SelectMUI } from "@material-tailwind/react";
+import Image from "next/image";
 
 interface LayoutProps {
   children: ReactNode;
@@ -17,15 +19,19 @@ const Layout = ({ children }: LayoutProps) => {
       </head>
       <body className="bg-gray-100 text-gray-900 h-screen font-inter">
         <div className="flex flex-col min-h-screen justify-between">
-          <header className="mx-auto max-w-screen-xl py-4 px-8 rounded-none flex items-center justify-between">
-            <a href="/" className="text-4xl font-bold text-gray700">
-              AI Prompt Generator
-            </a>
+          <header className="mx-auto max-w-screen-xl py-4 px-8 rounded-none justify-items-center bg-gray-200 w-full shadow-lg">
+            <Image
+              src={"/logo.png"}
+              alt={"AI Prompt Generator Logo"}
+              width={455}
+              height={79}
+              className="shadow-lg"
+            />
           </header>
           <main className="flex-grow p-4 flex items-center justify-center bg-gray-100">
             {children}
           </main>
-          <footer className="bg-white text-gray-600 py-4 text-center shadow-inner">
+          <footer className="bg-gray-200 text-gray-400 py-4 text-center shadow-inner">
             <p>© 2024 AI Prompt Generator. All rights reserved.</p>
           </footer>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,10 @@ export default function HomePage() {
   const [prompt, setPrompt] = useState("");
   const [useCase, setUseCase] = useState("generalEnhancement");
   const [enhancedPrompt, setEnhancedPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleGeneratePrompt = async () => {
+    setIsLoading(true);
     try {
       const response = await fetch("/api/generate", {
         method: "POST",
@@ -34,63 +36,145 @@ export default function HomePage() {
     } catch (error) {
       console.error("Error generating prompt:", error);
       setEnhancedPrompt("Error: Unable to generate prompt.");
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <div className="flex items-center justify-center h-full min-w-full bg-gray-100">
+      {isLoading && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 transition-opacity duration-300 ease-in-out">
+          <Card className="p-6 rounded-lg flex items-center justify-center shadow-lg bg-white border border-gray-300">
+            <Typography
+              variant="h4"
+              className="text-[#9869b8] mb-4 text-center text-xl animate-pulse"
+            >
+              Generating Prompt...
+            </Typography>
+            <div className="w-full gap-x-2 flex justify-center items-center">
+              <div className="w-5 bg-[#d991c2] h-5 rounded-full animate-bounce"></div>
+              <div className="w-5 h-5 bg-[#9869b8] rounded-full animate-bounce [animation-delay:-.3s]"></div>
+              <div className="w-5 h-5 bg-[#6756cc] rounded-full animate-bounce [animation-delay:-.5s]"></div>
+            </div>
+          </Card>
+        </div>
+      )}
       <Card className="w-full max-w-2xl p-8 rounded-lg shadow-lg bg-white border border-purple-300">
-        <Typography
-          variant="h4"
-          className="mb-6 text-2xl text-center text-purple-700"
-        >
-          Your Personal Prompt Engineer
-        </Typography>
+        <div>
+          <Typography
+            variant="h4"
+            className="mb-6 text-2xl text-center text-purple-700"
+          >
+            Your Personal Prompt Engineer
+          </Typography>
+          <div className="w-full relative flex flex-col items-center justify-center pb-5">
+            <div className="absolute inset-x-auto top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-full blur-sm"></div>
+            <div className="absolute inset-x-auto top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-full"></div>
+            <div className="absolute inset-x-auto top-0 bg-gradient-to-r from-transparent via-purple-400 to-transparent h-[5px] w-1/2 blur-sm"></div>
+            <div className="absolute inset-x-auto top-0 bg-gradient-to-r from-transparent via-purple-400 to-transparent h-px w-1/2"></div>
+            <div className="absolute inset-0 w-full h-full bg-background [mask-image:radial-gradient(50%_200px_at_top,transparent_20%,white)]"></div>
+          </div>
+        </div>
         <CardBody>
           <div className="space-y-4">
-            <Typography variant="h6" className="text-purple-600">
+            <Typography variant="h6" className="text-purple-500">
               Select Use Case
             </Typography>
             <SelectMUI
               value={useCase}
               onChange={(value) => setUseCase(value || "")}
-              className="p-3 h-11 max-h-20 border border-purple-300 rounded-lg text-center bg-white text-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500"
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
+              className="p-3 h-11 max-h-20 border border-purple-300 rounded-lg bg-white text-gray-500 focus:outline-none focus:ring-2 focus:ring-purple-500 flex justify-items-center place-items-center"
+              // style={{
+              //   display: "flex",
+              //   alignItems: "center",
+              //   justifyContent: "center",
+              // }}
               menuProps={{
                 className:
-                  "bg-white text-gray-800 align-items-center justify-center",
+                  "bg-white text-gray-800 align-items-center justify-center ease-in-out duration-300 hover:cursor-pointer space-y-2",
               }}
             >
-              <OptionMUI value="generalEnhancement">
+              <OptionMUI
+                className="hover:bg-gray-200 transition-all px-2"
+                value="generalEnhancement"
+              >
                 Click to choose any use case...
               </OptionMUI>
-              <OptionMUI value="generalEnhancement">
+              <OptionMUI
+                className="hover:bg-gray-200 transition-all px-2"
+                value="generalEnhancement"
+              >
                 General Enhancement
               </OptionMUI>
-              <OptionMUI value="contentGeneration">
+              <OptionMUI
+                className="hover:bg-gray-200 transition-all px-2"
+                value="contentGeneration"
+              >
                 Content Generation
               </OptionMUI>
-              <OptionMUI value="codeReview">Code Review</OptionMUI>
-              <OptionMUI value="summarization">Summarization</OptionMUI>
-              <OptionMUI value="translation">Translation</OptionMUI>
-              <OptionMUI value="grammarCorrection">
+              <OptionMUI
+                className="hover:bg-gray-200 transition-all px-2"
+                value="codeReview"
+              >
+                Code Review
+              </OptionMUI>
+              <OptionMUI
+                className="hover:bg-gray-200 transition-all px-2"
+                value="summarization"
+              >
+                Summarization
+              </OptionMUI>
+              <OptionMUI
+                className="hover:bg-gray-200 transition-all px-2"
+                value="translation"
+              >
+                Translation
+              </OptionMUI>
+              <OptionMUI
+                className="hover:bg-gray-200 transition-all px-2"
+                value="grammarCorrection"
+              >
                 Grammar Correction
               </OptionMUI>
-              <OptionMUI value="customerServiceResponse">
+              <OptionMUI
+                className="hover:bg-gray-200 transition-all px-2"
+                value="customerServiceResponse"
+              >
                 Customer Service Response
               </OptionMUI>
-              <OptionMUI value="socialMediaPost">Social Media Post</OptionMUI>
-              <OptionMUI value="FAQGeneration">FAQ Generation</OptionMUI>
-              <OptionMUI value="emailDrafting">Email Drafting</OptionMUI>
-              <OptionMUI value="reportAnalysis">Report Analysis</OptionMUI>
-              <OptionMUI value="codeGeneration">Code Generation</OptionMUI>
+              <OptionMUI
+                className="hover:bg-gray-200 transition-all px-2"
+                value="socialMediaPost"
+              >
+                Social Media Post
+              </OptionMUI>
+              <OptionMUI
+                className="hover:bg-gray-200 transition-all px-2"
+                value="FAQGeneration"
+              >
+                FAQ Generation
+              </OptionMUI>
+              <OptionMUI
+                className="hover:bg-gray-200 transition-all px-2"
+                value="emailDrafting"
+              >
+                Email Drafting
+              </OptionMUI>
+              <OptionMUI
+                className="hover:bg-gray-200 transition-all px-2"
+                value="reportAnalysis"
+              >
+                Report Analysis
+              </OptionMUI>
+              <OptionMUI
+                className="hover:bg-gray-200 transition-all px-2"
+                value="codeGeneration"
+              >
+                Code Generation
+              </OptionMUI>
             </SelectMUI>
-
-            <Typography variant="h6" className="text-purple-600">
+            <Typography variant="h6" className="text-purple-500">
               Enter Prompt
             </Typography>
             <Textarea
@@ -104,7 +188,7 @@ export default function HomePage() {
           {/* /* From Uiverse.io by elijahgummer */}
           <button
             onClick={handleGeneratePrompt}
-            className="group/button relative inline-flex items-center justify-center overflow-hidden rounded-md bg-purple-800 backdrop-blur-lg px-6 py-2 text-base font-semibold text-white transition-all duration-300 ease-in-out hover:scale-104 hover:shadow-xl hover:shadow-gray-600/50 border border-white/20 w-full hover:bg-purple-600"
+            className="group/button relative inline-flex items-center justify-center overflow-hidden rounded-md bg-purple-600 backdrop-blur-lg px-6 py-2 text-base font-semibold text-white transition-all duration-300 ease-in-out hover:scale-104 hover:shadow-xl hover:shadow-gray-600/50 border border-white/20 w-full hover:bg-purple-500"
           >
             <span className="text-lg">Generate Enhanced Prompt</span>
             <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
@@ -112,7 +196,7 @@ export default function HomePage() {
             </div>
           </button>
           <div className="bg-white p-4 rounded-lg border border-purple-300 mt-4">
-            <Typography variant="h6" className="text-purple-600">
+            <Typography variant="h6" className="text-purple-500">
               Enhanced Prompt:
             </Typography>
             <Typography className="mt-2 text-gray-400">
